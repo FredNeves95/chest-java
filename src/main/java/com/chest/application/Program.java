8d5c5fa264ff44a0cab5e3cff2b89fe405d494a4
FredNeves95 <fredbneves95@gmail.com>
2024-03-22 22:28:06 -0300

    feat: start board instance

--- src/main/java/com/chest/application/Program.java
+++ src/main/java/com/chest/application/Program.java
@@ -2,14 +2,11 @@ package com.chest.application;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.chest.application.boardGame.Position;
-
+import com.chest.application.boardGame.Board;
 @SpringBootApplication
 public class Program {
 	public static void main(String[] args) {
-		Position pos = new Position(3, 5);
-
-		System.out.println(pos.toString());
+		Board board = new Board(8, 8);
 	}
 
 }
