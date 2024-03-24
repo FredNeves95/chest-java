c37bc5f408521a0644cebf38382eabe92227037d
FredNeves95 <fredbneves95@gmail.com>
2024-03-23 21:47:32 -0300

    feat: methods to get piece by row, column and position in board

--- src/main/java/com/chest/application/boardGame/Board.java
+++ src/main/java/com/chest/application/boardGame/Board.java
@@ -19,4 +19,11 @@ public class Board {
     return columns;
   }
 
+  public Piece piece(int row, int column){
+    return pieces[row][column];
+  }
+
+  public Piece piece(Position position){
+    return pieces[position.getRow()][position.getColumn()];
+  } 
 }
